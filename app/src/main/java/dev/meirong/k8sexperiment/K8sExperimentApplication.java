f253a3e8c60a23e4c612e4dbe3b52fecb2debd68
Meirong <meirongdev@gmail.com>
2026-03-11 16:39:23 +0800

    chore: bootstrap Spring Boot 3.5.1 + Java 25 project

---
+++ app/src/main/java/dev/meirong/k8sexperiment/K8sExperimentApplication.java
@@ -0,0 +1,13 @@
+package dev.meirong.k8sexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sExperimentApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/dev/meirong/k8sexperiment/K8sExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.meirong.k8sexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
